c61cd384e80ba9c71c2d36a437a6082790c111f1
aedior <aliroide570@gmail.com>
2024-12-16 16:48:10 +0330

    ...

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/edior/Documents/niaz/niazFarmesi-server/venv"
+set -gx VIRTUAL_ENV "/home/edior/Documents/project/niaz/django-niaz/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
