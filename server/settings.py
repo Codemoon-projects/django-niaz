3c9f51d31111623a30b543bebe6bc75f3a090751
algonouir <algo.mahdi.nouri@gmail.com>
2025-03-15 14:33:00 +0330

    Update CORS allowed origins and adjust virtual environment paths

--- server/settings.py
+++ server/settings.py
@@ -97,8 +97,8 @@ ALLOWED_HOSTS = ["*"]
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://niaz.liara.run"
-    "https://niazpharmacy.ir/"
+    "https://niaz.liara.run",
+    "https://niazpharmacy.ir",
 ]
 
 
